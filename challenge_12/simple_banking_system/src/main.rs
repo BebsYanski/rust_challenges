5020b7d75310c4f2393856cb72ce950c75dc1f59
BebsYanski <bebongnchu.yannick@isparkinno.com>
2026-04-17 04:11:10 +0100

    feat: add simple banking system with user and account structures

---
+++ challenge_12/simple_banking_system/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+struct User {
+    id: u32,
+    name: String,
+    age: u8,
+    tel: u128,
+}
+struct BankAccount {
+    id: u32,
+    owner: User,
+    balance: f64,
+}
+
+struct Bank {
+    bank: HashMap<u32, BankAccount>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+impl Bank {
+    fn create_account() {
+        // TODO: Add a new user with a bank account + initial balance
+        todo!("Create Accounts");
+    }
+
+    fn deposit() {
+        // TODO: Increase the balance of a specific account
+        todo!("Deposit into Accounts");
+    }
+
+    fn withdraw() {
+        // TODO: Decrease balance (but only if theu have enough money)
+        todo!("Withdraw from Accounts");
+    }
+    fn check_balance() {
+        // TODO: View the current status of an account
+        todo!()
+    }
+}
